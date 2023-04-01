e329dccf024892bc2c8fd3c397ad150e57382949
Alejo Flores Lucey <alejo@misterflores.com>
2023-04-01 19:02:00 -0300

    Fix scrolling issue due to navbar

--- front/pages/component/navBar.js
+++ front/pages/component/navBar.js
@@ -1,12 +1,11 @@
-
 function NavBar(){
     return(
-        <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{backgroundColor: "#ffffff"}}>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top" style={{backgroundColor: "#ffffff"}}>
         <div className="container-fluid d-flow justify-content-center">
             <a className="navbar-brand" href="#">Kanbana</a>
         </div>
         </nav>
     );
-    }
+}
 
-    export default NavBar;
+export default NavBar;
