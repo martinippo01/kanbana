35bb6eb326b7cdccbcf291bc43489b126d22f90d
Alejo Flores Lucey <alejo@misterflores.com>
2023-04-01 20:46:46 -0300

    Add disabled logic to New Task Modal

--- front/pages/projectView/index.js
+++ front/pages/projectView/index.js
@@ -189,7 +189,12 @@ export default function projectView() {
                                             </Draggable>
                                         )}
                                         <div className="d-grid gap-2">
-                                            <Button variant="secondary" onClick={handleShowModal}>+</Button>
+                                            <Button variant="secondary" onClick={handleShowModal}>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-plus-circle" viewBox="0 0 16 16">
+                                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                                                </svg>
+                                            </Button>
                                         </div>
                                     </Card.Body>
                                 </Card>
@@ -242,7 +247,7 @@ export default function projectView() {
                         </Form>
                     </Modal.Body>
                     <Modal.Footer>
-                    <Button variant="primary" onClick={handleAddTask}>
+                    <Button variant="primary" onClick={handleAddTask} disabled={(newTaskDescription != "" && newTaskAssignee != "") ? false : true}>
                         Save
                     </Button>
                     </Modal.Footer>
