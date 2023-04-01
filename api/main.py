f6321f3c8bd5a598a7b0a97e10ac2ec8c158f23a
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-04-01 16:58:18 -0300

    Develop processing

--- api/main.py
+++ api/main.py
@@ -3,7 +3,9 @@ from pydantic import BaseModel
 from typing import List
 import openai
 
-openai.api_key = "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"
+# openai.organization = "org-Hz3NVSoiYXLROi1jFcp14Gzg"
+openai.api_key = "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"
+
 
 app = FastAPI()
 
@@ -14,15 +16,32 @@ class Person(BaseModel):
 class Project(BaseModel):
     people: List[Person]
     name: str
-    description: List[str]
+    purpose: str
+    features: str
+    audience: str
+    requirements: str
+    comments: str
 
-@app.post("/")
-async def create_project(project: Project):
+def get_prompt(project: Project) -> str:
+    prompt = "Based on the following information of a software project, generate a list of tasks to develop the project. Then, from the list of persons in the team, assign these tasks equally to the team, based on each person set of skills. The response must only contain the data in a JSON object style, as an array of this object: {\"task\": <string>, \"assignee\": <string>}. Please do not add any explanation or title, only the array. \n\n" + \
+    "Project Information:\n\
+    • Purpose of the project: " + project.purpose + \
+    "\n • Key Features of the project: " + project.features + \
+    "\n • Target Audience of the project: " + project.audience + \
+    "\n • Technical Requirements: " + project.requirements + \
+    "\n • Additional Comments: " + project.comments + \
+    "\n\n Team Members and Skillset:\n"
+    for person in project.people:
+        prompt += f"• {person.name}: {','.join(person.skills)}\n"
+
+    return prompt
+
+def ask_chatgpt(prompt: str) -> str:
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
                 {"role": "system", "content": "You are a chatbot"},
-                {"role": "user", "content": "Why should DevOps engineer learn kubernetes?"},
+                {"role": "user", "content": prompt},
             ]
     )
 
@@ -30,5 +49,11 @@ async def create_project(project: Project):
     for choice in response.choices:
         result += choice.message.content
 
-    print(result)
-    return project
+    return list(eval(result))
+
+
+
+@app.post("/")
+async def create_project(project: Project):
+    prompt = get_prompt(project)
+    return ask_chatgpt(prompt)
