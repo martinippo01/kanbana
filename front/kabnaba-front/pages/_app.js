0e41ca43c75e7d4f7603734f5c4f3ea59acac95b
martinippo01 <84152110+martinippo01@users.noreply.github.com>
2023-04-01 04:10:17 -0300

    Routing done and bootstrap installed

--- front/kabnaba-front/pages/_app.js
+++ front/kabnaba-front/pages/_app.js
@@ -1,3 +1,8 @@
+// add bootstrap css
+import 'bootstrap/dist/css/bootstrap.css'
+// own css files here
+import "../css/customcss.css";
+
 import '<kanbana-front>/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
