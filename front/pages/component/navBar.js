420b55358abcb2de3941c522ca7d20f65dcaec64
NehuenLlanos <nehuengllanos@gmail.com>
2023-04-02 00:31:32 -0300

    Navbar changes and footer added.

--- front/pages/component/navBar.js
+++ front/pages/component/navBar.js
@@ -1,10 +1,9 @@
+import Navbar from 'react-bootstrap/Navbar';
 function NavBar(param){
     return(
-        <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top" style={{backgroundColor: param.color}}>
-        <div className="container-fluid d-flow justify-content-center">
-            <a className="navbar-brand" href="#">Kanbana</a>
-        </div>
-        </nav>
+        <Navbar  bg="dark" variant="dark" className='d-flex justify-content-center'>
+            <Navbar.Brand className="my-auto" href="#home">Kanbana</Navbar.Brand>
+        </Navbar>
     );
 }
 
