5b82e937ddcc2a976dc164cdc3eb6d4da28c7068
Alejo Flores Lucey <alejo@misterflores.com>
2023-04-02 02:55:14 -0300

    Final tweaks to frontend

--- front/pages/component/navBar.js
+++ front/pages/component/navBar.js
@@ -1,8 +1,16 @@
 import Navbar from 'react-bootstrap/Navbar';
-function NavBar(param){
+
+function NavBar() {
     return(
-        <Navbar  bg="dark" variant="dark" className='d-flex justify-content-center'>
-            <Navbar.Brand className="my-auto" href="#home">Kanbana</Navbar.Brand>
+        <Navbar bg="dark" variant="dark" className='d-flex justify-content-center'>
+            <Navbar.Brand className="my-auto" href="/">
+                <img
+                    src="/logo.png"
+                    height="30"
+                    className="d-inline-block align-top"
+                    alt="Kanbana"
+                />
+            </Navbar.Brand>
         </Navbar>
     );
 }
